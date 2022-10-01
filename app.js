bedbf55569cd7129969e39df63ab88bd8921b489
benni maxon <benni.maxon@gmail.com>
2022-10-01 15:53:26 -0700

    input added

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // grab DOM elements
 
 // set event listeners
-const nameDisplay = document.getElementById('name-display')
-console.log(nameDisplay)
+const nameDisplay = document.getElementById('name-display');
+console.log(nameDisplay);
 
 //
 nameDisplay.textContent = 'Benni';
-nameDisplay.classList.add('purple')
+nameDisplay.classList.add('black');
+
+const updateBtn = document.getElementById('updateBtn');
+updateBtn.addEventListener('click', () => {
+    console.log('I am clicking the button');
+    const nameInput = document.getElementById('nameInput');
+    nameDisplay.textContent = nameInput.value;
+});
