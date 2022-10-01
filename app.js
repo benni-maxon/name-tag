f80b7b42ddb463b2db208b56da8e90b41abfaf2c
benni maxon <benni.maxon@gmail.com>
2022-10-01 15:22:29 -0700

    name and js stuff

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 // grab DOM elements
 
 // set event listeners
+const nameDisplay = document.getElementById('name-display')
+console.log(nameDisplay)
+
+//
+nameDisplay.textContent = 'Benni';
+nameDisplay.classList.add('purple')
